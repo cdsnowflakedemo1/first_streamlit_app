8519ef20b55ce118d340ce45607e1fd840234895
Peggy <114204879+cdsnowflakedemo1@users.noreply.github.com>
2022-09-22 23:36:19 -0400

    Update streamlit_app.py
    changes header to txt for sub menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.title('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 
-streamlit.title('Kale,Spinach & Rocket Smoothie')
+streamlit.text('Kale,Spinach & Rocket Smoothie')
 
-streamlit.title('Hard-Boiled Free-Range Egg')
+streamlit.text('Hard-Boiled Free-Range Egg')
