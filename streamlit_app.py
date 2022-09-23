5e7bdc9d7f275d79fee9ef429a0f306df90acf76
Peggy <114204879+cdsnowflakedemo1@users.noreply.github.com>
2022-09-22 23:36:45 -0400

    Update streamlit_app.py
    spacing

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 
-streamlit.text('Kale,Spinach & Rocket Smoothie')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
 
 streamlit.text('Hard-Boiled Free-Range Egg')
