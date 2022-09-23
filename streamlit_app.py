032a6f86ca1d42955e7e2bac8fef8691ab62b172
Peggy <114204879+cdsnowflakedemo1@users.noreply.github.com>
2022-09-22 23:35:21 -0400

    Update streamlit_app.py
    added menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.title('Omega 3 & Blueberry Oatmeal')
+
+streamlit.title('Kale,Spinach & Rocket Smoothie')
+
+streamlit.title('Hard-Boiled Free-Range Egg')
